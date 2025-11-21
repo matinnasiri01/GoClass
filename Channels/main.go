4b6bee10a7e4eb36f10a2252f77ed56130aa0d90
Matin Nasiri <mnasirii829@gmail.com>
2025-11-21 12:52:40 +0330

    channels update*

--- Channels/main.go
+++ Channels/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -36,4 +37,40 @@ func main() {
 
 	// ⚠ Sending to a closed channel causes panic
 	// ch <- 50 // -> panic: send on closed channel
+
+
+	// make readonly or ...
+	name := make(chan string , 1)
+	sendData(name,"Matin")
+	fmt.Printf("\nHellow %s",receiverData(name))
+
+
+
+	/// Select
+	ch1 := make(chan string,1)
+	ch2 := make(chan string,1)
+	go func ()  {
+		time.Sleep(1 * time.Second)
+		ch1 <- "Ch1"	
+	}()
+	go func ()  {
+		time.Sleep(2 * time.Second)
+		ch2 <- "Ch2"	
+	}()
+	for i := 0; i < 2; i++{
+		select{
+		case msg1 := <- ch1:
+			fmt.Println(msg1)
+		
+		case msg2 := <- ch1:
+			fmt.Println(msg2)
+		}
+	} 
+}
+func sendData (ch chan<- string,text string){
+	ch <- text
+}
+
+func receiverData(ch <-chan string) string{
+	return <-ch 
 }
