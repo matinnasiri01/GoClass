98dcdaacc6c1b9f35ae0d4f152eeb1e0b529a02d
Matin Nasiri <mnasirii829@gmail.com>
2026-01-01 09:11:24 +0330

    Import&Package

---
+++ Package/main.go
@@ -0,0 +1,20 @@
+// Page 101
+
+
+package main
+
+import (
+   // Global import
+   "fmt"
+   // Blank Import
+   _ "github.com/stretchr/testify"
+   // Nested Packages
+   _ "math/rand"
+)
+
+
+func main() {
+   fmt.Println("Hello")
+
+	
+}
