7ee88f8ec8f751e75ccf4927999e49ce68ff76ac
Matin Nasiri <mnasirii829@gmail.com>
2026-02-06 16:32:38 +0330

    Template

--- Echo/main.go
+++ Echo/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"html/template"
+	"io"
 )
 
 // ----------------------------
@@ -43,15 +45,55 @@ func SayHiHandler(c echo.Context) error {
 func PathParameterHandler(c echo.Context) error {
 	name := c.Param("namep")
 	age := c.Param("agep")
-
+	
 	return c.String(
 		http.StatusOK,
 		fmt.Sprintf("Name : %s -----> Age -----> %s Grade -----> *****",
-			name, age))
+		name, age))
+	}
+
+
+
+// ----------------------------
+type TemplateRender struct {
+    templates *template.Template
 }
+func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+    return t.templates.ExecuteTemplate(w, name, data)
+}
+
+
+type Author struct {
+    FirstName string
+    LastName  string
+    Age       int
+}
+func aboutHandler(c echo.Context) error {
+    jkRowling := Author{
+        FirstName: "Joanne",
+        LastName:  "Rowling",
+        Age:       58,
+    }
+
+    data := map[string]interface{}{
+        "author": jkRowling,
+        "books": []string{"Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets",
+            "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire", "Harry Potter and the Order of the Phoenix",
+            "Harry Potter and the Half-Blood Prince", "Harry Potter and the Deathly Hallows",
+        },
+        "isFamous": true,
+    }
+    return c.Render(http.StatusOK, "about.html", data)
+}
+
 
 func main() {
 	e := echo.New()
+	e.Renderer = &TemplateRender{
+     	templates: template.Must(template.ParseGlob("templates/*.html")),
+     }
+	e.GET("/about", aboutHandler)
+
 	// Base:
 	e.GET("/", WelcomeHandler)
 	e.POST("/signup", SignupHandler)
